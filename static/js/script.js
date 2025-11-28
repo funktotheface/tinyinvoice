f562a105b4a59907f8836f3fe2827ff55fdc7af9
Douglas <douglas.mottershead@outlook.com>
2025-11-28 12:02:12 +0000

    increase speed of particle movement from 0.1 - 0.2

--- static/js/script.js
+++ static/js/script.js
@@ -158,7 +158,7 @@ particlesJS("particles-js", {
     },
     "move": {
       "enable": true,
-      "speed": 0.1,
+      "speed": 0.2,
       "direction": "none",
       "random": true,
       "straight": false,

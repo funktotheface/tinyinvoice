91d8ce88ea7590b947e748cbdd7aa989d4fcceb0
Douglas <douglas.mottershead@outlook.com>
2025-12-08 15:03:39 +0000

    add changes to user model, auth route and login html template to integrate google one tap login!

--- models.py
+++ models.py
@@ -11,7 +11,8 @@ class User(UserMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=True)
+    google_sub = db.Column(db.String(255), unique=True, nullable=True)
     plan = db.Column(db.String(50), default='free')
     stripe_id = db.Column(db.String(120))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
